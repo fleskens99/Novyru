3c20da04ab2183402f13300193ab3c289a18d03a
francisco Pires de Lima Fleskens <568311@student.fontys.nl>
2026-09-24 14:54:45 +0200

    Initial commit

---
+++ src/main/java/org/example/novyru/NovyruApplication.java
@@ -0,0 +1,13 @@
+package org.example.novyru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovyruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovyruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/novyru/NovyruApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.novyru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovyruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
